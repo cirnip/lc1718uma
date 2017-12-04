1c500cf02f2ebfb18a3601145d5a419d913af19b
sfxpok <ricardo@airmail.cc>
2017-12-04 19:23:23 +0000

    operações e o literal adicionados

--- lc.pl
+++ lc.pl
@@ -7,3 +7,15 @@ Trabalho de avaliação
 2017/2018
 
 */
+
+/* código escrito com base no que foi lido nas sugestões do enunciado */
+
+:-op(100, fy, 'neg').
+:-op(200, xfy, 'e').
+:-op(300, xfy, 'ou').
+:-op(400, xfy, 'imp').
+
+simb_prop(X) :- not(X = neg Y), not(X = Y e Z), not(X = Y ou Z).
+
+literal(X) :- simb_prop(X).
+literal(neg X) :- simb_prop(X).
