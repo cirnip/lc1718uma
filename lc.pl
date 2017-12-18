ae4b415ffc19fcac7aee189435642ff9b1d522f8
sfxpok <ricardo@airmail.cc>
2017-12-18 17:19:29 +0000

    comentários adicionados

--- lc.pl
+++ lc.pl
@@ -8,19 +8,33 @@ Trabalho de avaliação
 
 */
 
+/* A lista tem um elemento igual a X? 
+* Exemplo de input do predicado -> membro(1,[2,1]).
+* */
+
 membro(X, [X | _]).
 membro(X, [_ | C]):-membro(X, C).
+
+/* A lista elimina X, sse X estiver na lista
+* Exemplo de input do predicado -> elimina(1,[5,1],X). ou elimina(1,[5,1],[5].
+* */
 
 elimina(X,[],[]).
 elimina(X,[X|L],L1):- elimina(X,L,L1).
 elimina(X,[Y|L],[Y|L1]):- not(Y=X), elimina(X,L,L1).
+
+/* Operações lógicas */
 
 :-op(100, fy, 'neg').
 :-op(200, xfy, 'e').
 :-op(300, xfy, 'ou').
 :-op(400, xfy, 'imp').
 
+/* Definição de um símbolo proposicional com as operações lógicas */
+
 simb_prop(X):-not(X=neg Y),not(X=Y e Z),not(X=Y ou Z).
+
+/* Definição de um literal */
 
 literal(X):-simb_prop(X).
 literal(neg X):-simb_prop(X).
@@ -31,6 +45,7 @@ disj(X ou Y):-disj(X),disj(Y).
 fnc(X):-disj(X).
 fnc(X e Y):-fnc(X),fnc(Y).
 
+/* O predicado converte_imp_aux/2 definido abaixo é (...por ser preenchido) */
 
 converte_imp_aux(L,C):-membro(S,L), membro(X imp Y,S), elimina(S,L,L1), elimina(X imp Y, S, S1),
                     converte_imp_aux([[neg X,Y|S1]|L1],C).
@@ -40,9 +55,15 @@ converte_imp_aux(L,C):-membro(S,L), membro(neg(X imp Y),S), elimina(S,L,L1), eli
 
 converte_imp_aux(L,L).
 
+/* O predicado converte_imp/1 definido abaixo recebe uma expressão sse a mesma tiver uma implicação, a fim de ser simplificada. Grande parte do processo é feito pelo predicado converte_imp_aux/2 definido acima.
+* Exemplo de input do predicado -> converte_imp([[p imp q]]).
+* !!! NOTA: Não tenho a certeza se o predicado converte_imp/1 tem esta intenção !!! */
+
 converte_imp(L):- converte_imp_aux(L,C), write('A fórmula representada pela lista de listas introduzida é equivalente à fórmula que é representada pela seguinte lista de listas de fórmulas:'), nl, write(C).
 
-%EX1 - projecto%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Exercício 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/* Não sei. Dado um input do tipo "p ou q" ou "p imp q" devolve o mesmo input no output e um "true". */
 
 converte_aux(L,C):-membro(S,L),membro(neg neg X,S), elimina(S,L,L1),elimina(neg neg X,S,S1),
        converte_aux([[X|S1]|L1],C).
@@ -62,8 +83,10 @@ converte_aux(L,L).
 
 converte(L):-converte_aux(L,C),nl,write(C).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Exercício 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%ex 2 do projecto%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Exercício 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 elimina_rep_novo([[X],[X]],[[]]).
 elimina_rep_novo([[X|L],[X|S]],C):-membro([X],[L|S]),elimina_rep_novo([L|S],C).
 elimina_rep_novo([[X|L]],C):-not(membro([X],[L])),elimina_rep_novo([L],C).
@@ -96,3 +119,5 @@ ref_aux(L,[[]]):-res_aux(L,[[]]),nl,write('é refutavel').
 ref_aux(L,S):-res_aux(L,S),nl,write('não é refutavel').
 
 ref(L):-ref_aux(L,_),res(L).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Exercício 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
