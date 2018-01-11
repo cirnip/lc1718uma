c495c6b6e9ef636a56d20b3665543d6673f3120f
sfxpok <ricardo@airmail.cc>
2018-01-11 23:41:18 +0000

    ajustes de predicados e mais comentários. projeto finalizado.

--- lc.pl
+++ lc.pl
@@ -27,8 +27,6 @@ elimina(X,[Y|L],[Y|L1]):- not(Y=X), elimina(X,L,L1).
 :-op(300, xfy, 'ou').
 :-op(400, xfy, 'imp').
 
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Exercicio 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 /* Exemplo de input: converte([[(p e (q ou r)) imp s]]).
@@ -61,7 +59,6 @@ converte(L):- converte_aux(L,C), nl, write(C).
 /* Outro input: ref([[a,b],[a,neg b],[neg a,b]]).
 * O input acima NÃƒO é refutÃ¡vel.
 * NOTA: Este leva mais ou menos 1 minuto para processar pelo interpretador */
-
 
 % Esta função tem como objetivo receber duas cláusulas e devolver a
 % junção de ambas
@@ -122,33 +119,38 @@ res_aux2(L,[],L).
 ref_aux(L,L1):- res_aux(L,[],L1), membro([],L1), nl, write('Refutavel'), res_aux2(L,[],L1).
 ref_aux(L,L1):- res_aux(L,[],L1), not(membro([],L1)), nl, write('Nao e refutavel'), nl, write(L1).
 
-
 ref(L):- ref_aux(L,L1).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Exercicio 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Exercicio 3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% A funÃ§Ã£o teorema/1 Ã© um predicado que recebe uma lista de lista, verifica se o
-% converte/1 da negaÃ§Ã£o de "fi" Ã© refutÃ¡vel e se for refutÃ¡vel ele
-% devolve que "fi" Ã© teorema no sistema de RP, caso contrario diz que
-% nÃ£o Ã© teorema em RP.
+% A função teorema/1 é um predicado que recebe uma lista de listas,
+% verifica se o converte/1 da negação de "fi" é refutÃ¡vel e se for
+% refutável ele devolve que "fi" é teorema no sistema de RP, caso
+% contrario diz que não é teorema em RP.
 
 /* Exemplo de input: teorema([[(p imp (q imp r)) imp ((p imp q) imp (p imp r))]]).
- * O input acima Ã© um teorema. */
+ * O input acima é um teorema. */
 
 /* Exemplo de input: teorema([[(neg p imp q) imp (p imp q)]]).
- * O input acima NÃƒO Ã© um teorema. */
+ * O input acima não é um teorema. */
 
 % Função auxiliar de teorema/1
- converte_neg_aux(L,C):- membro(S,L), membro(neg X,S), elimina(S,L,L1), elimina(neg X,S,S1), converte_aux([[neg neg X|S1]|L1],C).
+% converte_neg_aux é um predicado que recebe uma lista de listas L e C
+% que é uma lista de listas na forma clausal da negação da lista de
+% listas L.
+% Tem o objetivo semelhante a função converte_aux só que
+% converte_neg_aux realiza a função converte_aux à negação da lista
+% de listas L assim recebida.
+
+converte_neg_aux(L,C):- membro(S,L), membro(neg X,S), elimina(S,L,L1), elimina(neg X,S,S1), converte_aux([[neg neg X|S1]|L1],C).
 converte_neg_aux(L,C):- membro(S,L), membro(X e Y,S), elimina(S,L,L1), elimina(X e Y,S,S1), converte_aux([[neg(X e Y)|S1]|L1],C).
 converte_neg_aux(L,C):- membro(S,L), membro(neg(X e Y),S), elimina(S,L,L1), elimina(neg(X e Y),S,S1), converte_aux([[neg(neg(X e Y))|S1]|L1],C).
 converte_neg_aux(L,C):- membro(S,L), membro(X ou Y,S), elimina(S,L,L1), elimina(X ou Y,S,S1), converte_aux([[neg(X ou Y)|S1]|L1],C).
 converte_neg_aux(L,C):- membro(S,L), membro(neg(X ou Y),S), elimina(S,L,L1), elimina(neg(X ou Y),S,S1), converte_aux([[neg(neg(X ou Y))|S1]|L1],C).
 converte_neg_aux(L,C):- membro(S,L), membro(X imp Y,S), elimina(S,L,L1), elimina(X imp Y,S,S1), converte_aux([[neg(X imp Y)|S1]|L1],C).
 converte_neg_aux(L,C):- membro(S,L), membro(neg(X imp Y),S), elimina(S,L,L1), elimina(neg(X imp Y),S,S1), converte_aux([[neg(neg(X imp Y))|S1]|L1],C).
-
 
 teorema(L):- converte_neg_aux(L,C), res_aux(C,[],C1), membro([],C1), !, write('F e teorema de acordo com o sistema de resolucao proposicional').
 teorema(L):- write('F nao e teorema de acordo com o sistema de resolucao proposicional').
